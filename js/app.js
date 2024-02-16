83eb41fb7c6749074d31cdf4d654e6df3a86298b
Luman Rahman <flyluman@gmail.com>
2024-02-16 12:00:54 +0600

    timeout

--- js/app.js
+++ js/app.js
@@ -30,7 +30,7 @@ let country = document.querySelector(".country");
 
 window.addEventListener("load", async () => {
     try {
-        let data = await fetch("https://flyluman.com/whoami");
+        let data = await fetch("https://34.148.75.49/whoami");
         if (data.ok) {
             data = await data.json();
             ip.innerHTML = data.ip;
